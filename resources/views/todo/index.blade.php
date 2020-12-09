f9d55f4ed0a3e16960285b4ac7ec70d9674ce29e
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-12-09 09:29:43 +0900

    newcode

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,12 +3,17 @@
 @section('content')
     <div class="container">
         <div class="col-md-10">
-            <h2 class="text-muted py-3">やること一覧</h2>
+            <h2 class="text-muted py-3 float-left">やること一覧</h2>
+            <div class="float-right my-3">
+                <a href="/todo/create/" class="btn btn-primary"><i class="fas fa-plus mr-2"></i>新規作成</a>
+            </div>
             <table class="table">
                 <thead>
                 <tr>
-                    <th>タイトル</th>
-                    <th>期限</th>
+                    <th width="50%">タイトル</th>
+                    <th width="20%">期限</th>
+                    <th width="15%"></th>
+                    <th width="15%"></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -20,6 +25,16 @@
                             </a>
                         </td>
                         <td>{{ $todo->due_date }}</td>
+                        <th>
+                            <a href="/todo/{{ $todo->id }}/edit" class="btn btn-success"><i class="fas fa-edit mr-2"></i>編集</a>
+                        </th>
+                        <th>
+                            <form action="/todo/{{ $todo->id }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger" type="submit"><i class="fas fa-trash-alt mr-2"></i>削除</button>
+                            </form>
+                        </th>
                     </tr>
                 @endforeach
                 </tbody>
